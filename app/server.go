94179a794b07d3e7a896739b900f846f3d6a3177
Dan Quan <dan@quan.io>
2023-12-26 13:45:56 -0800

    Add expiration

--- app/server.go
+++ app/server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -20,12 +23,17 @@ func main() {
 	server.listenAndServe()
 }
 
+type value struct {
+	v          []byte
+	expiration *int64
+}
+
 type server struct {
 	l net.Listener
 
 	db *struct {
 		sync.RWMutex
-		m map[string][]byte
+		m map[string]*value
 	}
 }
 
@@ -64,34 +72,61 @@ func (s *server) handleConn(conn net.Conn) {
 					input = input[1:]
 				case "ECHO":
 					echo := input[1].(string)
-
-					conn.Write([]byte("+"))
-					conn.Write([]byte(echo))
-					conn.Write([]byte("\r\n"))
+					conn.Write(encodeString(echo))
 
 					input = input[2:]
 				case "SET":
 					key := input[1].(string)
-					value := input[2].(string)
+
+					v := &value{
+						v: []byte(input[2].(string)),
+					}
+
+					input = input[3:]
+
+					if len(input) >= 1 {
+						if val, ok := input[0].(string); ok {
+							if strings.ToUpper(val) == "PX" {
+								now := time.Now().UnixNano() / int64(time.Millisecond)
+								expiration, err := strconv.Atoi(input[1].(string))
+								exp := int64(expiration) + now
+
+								if err != nil {
+									fmt.Println("Error getting expiration")
+									os.Exit(1)
+								}
+								v.expiration = &exp
+
+								input = input[2:]
+							}
+						}
+					}
 
 					s.db.Lock()
-					s.db.m[key] = []byte(value)
+					s.db.m[key] = v
 					s.db.Unlock()
 
 					conn.Write([]byte("+OK\r\n"))
 
-					input = input[3:]
-
 				case "GET":
 					key := input[1].(string)
+					now := time.Now().UnixNano() / int64(time.Millisecond)
 
-					s.db.RLock()
+					s.db.Lock()
 					val := s.db.m[key]
-					s.db.RUnlock()
 
-					conn.Write([]byte("+"))
-					conn.Write(val)
-					conn.Write([]byte("\r\n"))
+					if val != nil && val.expiration != nil && *val.expiration <= now {
+						s.db.m[key] = nil
+						val = nil
+					}
+
+					s.db.Unlock()
+
+					if val == nil {
+						conn.Write([]byte("$-1\r\n"))
+					} else {
+						conn.Write(encodeString(string(val.v)))
+					}
 
 					input = input[2:]
 				}
@@ -102,6 +137,23 @@ func (s *server) handleConn(conn net.Conn) {
 	}
 }
 
+func encodeString(s string) []byte {
+	buf := new(bytes.Buffer)
+	body := []byte(s)
+
+	buf.WriteByte('$')
+
+	length := strconv.Itoa(len(body))
+
+	buf.WriteString(length)
+	buf.Write([]byte("\r\n"))
+
+	buf.Write(body)
+	buf.Write([]byte("\r\n"))
+
+	return buf.Bytes()
+}
+
 func startServer(port string) (*server, error) {
 	l, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
@@ -110,8 +162,8 @@ func startServer(port string) (*server, error) {
 
 	var db = struct {
 		sync.RWMutex
-		m map[string][]byte
-	}{m: make(map[string][]byte)}
+		m map[string]*value
+	}{m: make(map[string]*value)}
 
 	return &server{
 		l:  l,
--- app/server_test.go
+++ app/server_test.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"net"
 	"testing"
+	"time"
 )
 
 func Test_handleConnectionPing(t *testing.T) {
@@ -52,13 +53,51 @@ func Test_handleEcho(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	reply, err := bufio.NewReader(conn2).ReadString('\n')
+	reader := bufio.NewReader(conn2)
+	reply, err := reader.ReadString('\n')
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if reply != "+hey\r\n" {
-		t.Errorf("Expected '+hey\r\n', but got '%s'", reply)
+	if reply != "$3\r\n" {
+		t.Errorf("Expected '$3\r\n', but got '%s'", reply)
+	}
+
+	reply, err = reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "hey\r\n" {
+		t.Errorf("Expected 'hey\r\n', but got '%s'", reply)
+	}
+}
+
+func Test_handleGetWithoutAMatch(t *testing.T) {
+	conn1, conn2 := net.Pipe()
+	defer conn1.Close()
+	defer conn2.Close()
+
+	s, err := startServer("0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go s.handleConn(conn1)
+
+	_, err = conn2.Write([]byte("*2\r\n$3\r\nGET\r\n$3\r\nhey\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := bufio.NewReader(conn2)
+	reply, err := reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "$-1\r\n" {
+		t.Errorf("Expected '$-1\r\n', but got '%s'", reply)
 	}
 }
 
@@ -93,12 +132,116 @@ func Test_handleSetGet(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	reply, err = bufio.NewReader(conn2).ReadString('\n')
+	reader := bufio.NewReader(conn2)
+	reply, err = reader.ReadString('\n')
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if reply != "+bye\r\n" {
-		t.Errorf("Expected '+bye\r\n', but got '%s'", reply)
+	if reply != "$3\r\n" {
+		t.Errorf("Expected '$3\r\n', but got '%s'", reply)
+	}
+
+	reply, err = reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "bye\r\n" {
+		t.Errorf("Expected 'bye\r\n', but got '%s'", reply)
+	}
+}
+
+func Test_handleSetGetWithExpirationButNotExpired(t *testing.T) {
+	conn1, conn2 := net.Pipe()
+	defer conn1.Close()
+	defer conn2.Close()
+
+	s, err := startServer("0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go s.handleConn(conn1)
+
+	_, err = conn2.Write([]byte("*5\r\n$3\r\nSET\r\n$3\r\nhey\r\n$3\r\nbye\r\n$2\r\npx\r\n$4\r\n1000\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reply, err := bufio.NewReader(conn2).ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "+OK\r\n" {
+		t.Errorf("Expected '+OK\r\n', but got '%s'", reply)
+	}
+
+	_, err = conn2.Write([]byte("*2\r\n$3\r\nGET\r\n$3\r\nhey\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := bufio.NewReader(conn2)
+	reply, err = reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "$3\r\n" {
+		t.Errorf("Expected '$3\r\n', but got '%s'", reply)
+	}
+
+	reply, err = reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "bye\r\n" {
+		t.Errorf("Expected 'bye\r\n', but got '%s'", reply)
+	}
+}
+
+func Test_handleSetGetWithExpirationAndExpired(t *testing.T) {
+	conn1, conn2 := net.Pipe()
+	defer conn1.Close()
+	defer conn2.Close()
+
+	s, err := startServer("0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go s.handleConn(conn1)
+
+	_, err = conn2.Write([]byte("*5\r\n$3\r\nSET\r\n$3\r\nhey\r\n$3\r\nbye\r\n$2\r\npx\r\n$1\r\n2\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reply, err := bufio.NewReader(conn2).ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "+OK\r\n" {
+		t.Errorf("Expected '+OK\r\n', but got '%s'", reply)
+	}
+
+	time.Sleep(10 * time.Millisecond)
+	_, err = conn2.Write([]byte("*2\r\n$3\r\nGET\r\n$3\r\nhey\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	reader := bufio.NewReader(conn2)
+	reply, err = reader.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reply != "$-1\r\n" {
+		t.Errorf("Expected '$-1\r\n', but got '%s'", reply)
 	}
 }
